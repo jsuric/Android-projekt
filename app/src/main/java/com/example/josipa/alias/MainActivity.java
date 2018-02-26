9f28c4c84c86a2557c6bac72dd10c4e220d6e963
Ana <Ana@nesto.com>
2018-02-26 23:00:21 +0100

    Add dialog with rules

--- app/src/main/java/com/example/josipa/alias/MainActivity.java
+++ app/src/main/java/com/example/josipa/alias/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.josipa.alias;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public void showRules(View view)
+    {
+        // 1. Instantiate an AlertDialog.Builder with its constructor
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+// 2. Chain together various setter methods to set the dialog characteristics
+        builder.setMessage(R.string.rulesString)
+                .setTitle(R.string.rules)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+// 3. Get the AlertDialog from create()
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+
+
 }
